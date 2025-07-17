465c48cac18814369ef5f7d39b2941460ddf08b6
jelenad6 <136815108+jelenad6@users.noreply.github.com>
2025-07-17 14:26:57 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App'; // Assuming your main App component is in App.js
+import App from './App'; 
 import './style.css';
 
 ReactDOM.render(
